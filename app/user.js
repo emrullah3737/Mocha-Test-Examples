8284116f2aefd47c9d173a3b3e88e04b16312854
Emrullah Karataş <emrullah@efabrika.com>
2017-04-25 12:47:54 +0300

    update

--- app/user.js
+++ app/user.js
@@ -1,12 +1,18 @@
-class User {
+'use strict'
+const Utils = require('./utils');
+class User extends Utils{
 
   constructor() {
+    super(5)
     console.log('constructor');
   }
 
   save(cb){
-    console.log('saved');
-    cb();
+    if(super.save() === true) {
+      cb(false);
+    } else {
+      cb(true);
+    }
     // cb('Error')
   }
 }
---
+++ app/utils.js
@@ -0,0 +1,10 @@
+'use strict'
+module.exports = class Utils{
+
+  constructor(sayi){
+    console.log(`Utils ${sayi}`);
+  }
+  save(){
+    return true;
+  }
+}
